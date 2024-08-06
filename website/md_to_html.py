312f51952a214af5d8d8e83e1f2c8743824f116f
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-08-06 20:26:28 +0000

    no need for a server so why have one

--- website/md_to_html.py
+++ website/md_to_html.py
@@ -1,3 +1,5 @@
+# this will be to transform markdown files to html files with our own styling and our own rules, the reason for this is that we might one day need more customisation options and it cant be that hard to make our own md to html
+
 
 def generate_html():
     print("generating html")
