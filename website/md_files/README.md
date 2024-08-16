facc47dac9abed190579ba474767af01ce08ada8
patcybermind <pato.gonzalezcabrera@gmail.com>
2024-08-16 14:18:18 -0400

    aug 16

---
+++ website/md_files/README.md
@@ -0,0 +1,5 @@
+---
+publish: false
+---
+# this is the directory where all posts will be written
+you can write a post of your own
--- website/md_to_html.py
+++ website/md_to_html.py
@@ -1,5 +1,10 @@
 # this will be to transform markdown files to html files with our own styling and our own rules, the reason for this is that we might one day need more customisation options and it cant be that hard to make our own md to html
-
+# when running this script you must be in the website directory and if you are on vscode you working terminal directory must be FPGA-YSWS/website
+import os
 
 def generate_html():
-    print("generating html")
+    list_of_files = [file for file in os.listdir("md_files") if file.endswith(".md") and file != "README.md"]
+    print(list_of_files)
+    
+
+generate_html()
